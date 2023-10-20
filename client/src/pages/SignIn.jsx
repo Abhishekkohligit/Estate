99f6baf73c832fc5fe88769835ac2943e328f008
Abhishek Kohli <ab02110211@gmail.com>
2023-10-20 05:01:53 +0100

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import {
+	signInStart,
+	signInSuccess,
+	signInFailure,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
-	const [formData, setFormData] = useState({}); // from submition
-	const [error, setError] = useState(null); //error handling
-	const [loading, setLoading] = useState(false); // set loading
+	const [formData, setFormData] = useState({}); // form submition
+	// const [error, setError] = useState(null); //error handling  --using redux now
+	// const [loading, setLoading] = useState(false); // set loading  --using redux now
+
+	const { loading, error } = useSelector((state) => state.user);// coming from global state now
 	const navigate = useNavigate(); //navigation post signup
+	const dispatch = useDispatch();
 	//getting user input & form data
 	const handleChange = (e) => {
 		setFormData({
@@ -18,7 +27,8 @@ export default function SignIn() {
 		e.preventDefault();
 		try {
 			// to prevent error crashing server
-			setLoading(true);
+			// setLoading(true);     ---using redux state now
+			dispatch(signInStart());
 			//create record
 			const res = await fetch("/api/auth/signin", {
 				method: "POST",
@@ -30,17 +40,21 @@ export default function SignIn() {
 			const data = await res.json();
 			//error handling
 			if (data.success === false) {
-				setError(data.message);
-				setLoading(false);
+				// setError(data.message);
+				// setLoading(false);  using redux state hooks now
+				dispatch(signInFailure(data.message));
 				return;
 			}
-			setLoading(false);
-			setError(null);
+			// setLoading(false);
+			// setError(null);  -- using redux hooks now
+
+			dispatch(signInSuccess(data));
 			navigate("/");
 			// console.log(data);
 		} catch (error) {
-			setLoading(false);
-			setError(error.message);
+			// setLoading(false);
+			// setError(error.message);  --- using redux hooks now
+			dispatch(signInFailure);
 		}
 	};
 	// }console.log(formData);
@@ -49,7 +63,6 @@ export default function SignIn() {
 			<h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
 
 			<form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-				
 				<input
 					type="email"
 					placeholder="Email"
