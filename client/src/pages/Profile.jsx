81a7c62124b6eeb18a88a1b5fbb7275ae435b022
Abhishek Kohli <ab02110211@gmail.com>
2023-10-31 10:06:52 +0000

    added DELETE Listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -142,6 +142,25 @@ export default function Profile() {
 		}
 	};
 
+	const handleListingDelete = async (listingId) => {
+		try {
+			const res = await fetch(`/api/listing/delete/${listingId}`, {
+				method: "DELETE",
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				console.log(data.message);
+				return;
+			}
+
+			setUserListing((prev) =>
+				prev.filter((listing) => listing._id !== listingId)
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="p-3 max-w-lg mx-auto">
 			<h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -256,7 +275,14 @@ export default function Profile() {
 								<p>{listing.name}</p>
 							</Link>
 							<div className="flex flex-col items-center">
-								<button className="text-red-600 uppercase">Delete</button>
+								<button
+									onClick={() => {
+										handleListingDelete(listing._id);
+									}}
+									className="text-red-600 uppercase"
+								>
+									Delete
+								</button>
 								<button className="text-emerald-600 uppercase">edit</button>
 							</div>
 						</div>
