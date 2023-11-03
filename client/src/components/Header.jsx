aa7393575e0793c4931df41f7444b08b213d50f1
Abhishek Kohli <ab02110211@gmail.com>
2023-11-03 02:47:38 +0000

    update Header serch form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
 	const { currentUser } = useSelector((state) => state.user);
+	const [searchTerm, setSearchTerm] = useState("");
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const urlParams = new URLSearchParams(window.location.search);
+		urlParams.set("searchTerm", searchTerm);
+		const searchQuery = urlParams.toString();
+		navigate(`/search?${searchQuery}`);
+	};
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const searchTermFromUrl = urlParams.get("searchTerm");
+		if (searchTermFromUrl) {
+			setSearchTerm(searchTermFromUrl);
+		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [location.search]);
 	return (
 		<header className="bg-blue-300 shadow-md">
 			<div className="flex justify-between max-w-6xl mx-auto p-3">
@@ -13,13 +33,22 @@ export default function Header() {
 						<span className="text-blue-500">Estate</span>
 					</h1>
 				</Link>
-				<form className="bg-blue-200 p-3 rounded-lg flex items-center">
+				<form
+					onSubmit={handleSubmit}
+					className="bg-blue-200 p-3 rounded-lg flex items-center"
+				>
 					<input
 						className="bg-transparent focus:outline-none w-24 sm:w-64"
 						type="text"
 						placeholder="Search..."
+						value={searchTerm}
+						onChange={(e) => {
+							setSearchTerm(e.target.value);
+						}}
 					/>
-					<FaSearch className="text-blue-500 " />
+					<button>
+						<FaSearch className="text-blue-500 cursor-pointer " />
+					</button>
 				</form>
 				<ul className="flex gap-4">
 					<Link to="/">
