6c7961b47d145c162f714054a582ee53f8c027bc
Abhishek Kohli <ab02110211@gmail.com>
2023-11-04 13:25:39 +0000

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 //mondodb connection
@@ -16,6 +17,8 @@ mongoose
 	.catch((err) => {
 		console.log(err);
 	});
+
+const __dirname = path.resolve();
 
 // starting server
 const app = express();
@@ -29,6 +32,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // error middleware
 app.use((err, req, res, next) => {
